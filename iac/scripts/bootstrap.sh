e17c017d190f287a218df637e500d0be265d6e1d
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-08-03 13:05:19 +0200

    feat: install docker-compose

--- iac/scripts/bootstrap.sh
+++ iac/scripts/bootstrap.sh
@@ -9,3 +9,15 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt update
 apt-cache policy docker-ce
 sudo apt install -y docker-ce
+sudo apt install -y nginx
+# https://certbot.eff.org/instructions
+sudo snap install core
+sudo snap refresh core
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+# TODO: domain and email should be args
+sudo certbot --nginx -d pixel.citylab-berlin.org -m hash@ff6347.email
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+git clone https://github.com/technologiestiftung/workadventure.git
